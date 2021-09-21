3cc4e5900e8495f28e05645e28e5e20bc5090786
Hillary <khan.hillary@gmail.com>
2021-09-21 19:52:58 -0400

    adds unit tests

--- application.py
+++ application.py
@@ -3,38 +3,40 @@ from db import init_db, db
 from werkzeug.utils import secure_filename
 from models import Img
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    init_db(app)
 
-init_db(app)
+    @app.route('/')
+    def hello():
+        return "Let's see some cats!"
 
-@app.route('/')
-def hello():
-    return "Let's see some cats!"
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload():
+        if request.method=='POST':
+            pic = request.files['pic']
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method=='POST':
-        pic = request.files['pic']
+            if not pic:
+                return 'No pic uploaded', 400
 
-        if not pic:
-            return 'No pic uploaded', 400
-
-        filename = secure_filename(pic.filename)
-        mimetype = pic.mimetype
-        img = Img(img=pic.read(), mimetype=mimetype, name=filename)
-        db.session.add(img)
-        db.session.commit()
-        
-        return 'Image successfully uploaded!', 200
-    return render_template('index.html')
+            filename = secure_filename(pic.filename)
+            mimetype = pic.mimetype
+            img = Img(img=pic.read(), mimetype=mimetype, name=filename)
+            db.session.add(img)
+            db.session.commit()
+            
+            return 'Image successfully uploaded!', 200
+        return render_template('index.html')
 
 
-@app.route('/<int:id>')
-def get_img(id):
-    img = Img.query.filter_by(id=id).first()
-    if not img:
-        return 'There is no image with that id', 404
-    return Response(img.img, mimetype=img.mimetype)
+    @app.route('/<int:id>')
+    def get_img(id):
+        img = Img.query.filter_by(id=id).first()
+        if not img:
+            return 'There is no image with that id', 404
+        return Response(img.img, mimetype=img.mimetype)
+
+    return app
--- test_app.py
+++ test_app.py
@@ -1,3 +1,50 @@
-import os
-import tempfile
+from db import init_db
 import pytest
+from application import create_app
+from models import Img
+from db import init_db
+
+@pytest.fixture
+def client():
+    app = create_app()
+
+    with app.test_client() as client:
+        with app.app_context():
+            init_db(app)
+        yield client
+    
+def test_hello(client):
+    """Tests output and status code for the main route"""
+    url = '/'
+    response = client.get(url)
+    assert response.get_data() == b"Let's see some cats!"
+    assert response.status_code == 200
+
+def test_get_img_valid_output(client):
+    """Tests output for the retrieving a valid image"""
+    valid_id = 1
+    url = '/' + str(valid_id)
+    response = client.get(url)
+    img = Img.query.filter_by(id=valid_id).first()
+    assert response.get_data() == img.img
+
+def test_get_img_valid_status(client):
+    """Tests status code for the retrieving a valid image"""
+    valid_id = 1
+    url = '/' + str(valid_id)
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_get_img_invalid_output(client):
+    """Tests output for the retrieving an invalid image"""
+    invalid_id = 5
+    url = '/' + str(invalid_id)
+    response = client.get(url)
+    assert response.get_data() == b"There is no image with that id"
+
+def test_get_img_invalid_status(client):
+    """Tests status code for the retrieving an invalid image"""
+    invalid_id = 5
+    url = '/' + str(invalid_id)
+    response = client.get(url)
+    assert response.status_code == 404
